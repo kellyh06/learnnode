1b51a9192a8937f0e5c7ab767d12c72ed378df44
Kelly Hurt <kelly.hurt@tptlive.ee>
2025-04-29 10:39:53 +0300

    Add longpolling example

--- bin/server.js
+++ bin/server.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 let messages = [];
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -14,6 +16,16 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     let date = req.query.date ?? null;
     let filteredMessagesd = messages.filter(message => message.date > new Date(date));
+    res.json(filteredMessagesd);
+});
+
+app.get('/longpoll', async (req, res) => {
+    let date = req.query.date ?? null;
+    let filteredMessagesd;
+    do {
+        await sleep(1000);
+        filteredMessagesd = messages.filter(message => message.date > new Date(date));
+    } while (filteredMessagesd.length === 0)
     res.json(filteredMessagesd);
 });
 
