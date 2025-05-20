4fd34e7b794671167dd8fe7159828ed2b367b4de
Kelly Hurt <kelly.hurt@tptlive.ee>
2025-05-20 11:18:05 +0300

    Add XKCDscraper.js

---
+++ bin/XKCDscraper.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import * as cheerio from 'cheerio';
+import fs from 'fs';
+
+function cacheGet(name){
+    if(fs.exsistsSync(`../cache/${name}.html`)) {
+        return fs.readFileSync(`../cache/${name}.html`)
+    }
+    return false;
+}
+
+function cacheSet(name, value){
+    if(!fs.existsSync('../cache')){
+        fs.mkdirSync('../cache');
+    }
+    fs.writeFileSync(`../cache/${name}.html`, value);
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+for (let i = 3091; i >= 3081; i--){
+    let data = cacheGet(i);
+    if(!data) {
+        console.log('LIVE REQUEST!!!!!');
+        await sleep(1000);
+        let res = await axios.get(`https://xkcd.com/${i}/`);
+        data = res.data;
+        cacheSet(i, data);
+    }
+    
+    //console.log(res.data);
+    const $ = cheerio.load(res.data);
+    let src = $('#comi>img').attr('src');
+    let text = $('#comi>img').attr('title');
+    let title = $('#comi>img').attr('alt');
+
+    console.log(src);
+    console.log(text);
+    console.log(title);
+
+}
