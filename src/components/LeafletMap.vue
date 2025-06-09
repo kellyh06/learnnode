96b75ccb48baf7d3da24e325377118f0ba42f6b3
kellyh06 <kelly.hurt@tptlive.ee>
2025-06-09 20:48:35 +0300

    add a go to home button, a marker over the home and a polygon around it

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -10,17 +10,35 @@ const id = 'map-' + useId();
 let map;
 onMounted(() => {
     map = L.map(id).setView(center, zoom);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    var marker = L.marker([58.73120, 24.40793]).addTo(map);
+
+    // Kodu koordinaadid
+    const homeCoords = [58.73120, 24.40793];
+
+    L.marker(homeCoords).addTo(map);
+
+    const polygonCoords = [
+        [58.73150, 24.40760], // ülemine vasak
+        [58.73090, 24.40760], // alumine vasak
+        [58.73090, 24.40830], // alumine parem
+        [58.73150, 24.40830]  // ülemine parem
+    ];
+    L.polygon(polygonCoords, {
+        color: 'blue',
+        fillColor: '#3f51b5',
+        fillOpacity: 0.3
+    }).addTo(map);
 });
+
 watch(() => center, (center, oldCenter) => {
     console.log(center, oldCenter);
     map.panTo(center);
 });
- 
+
 watch(() => zoom, (zoom) => {
     map.setZoom(zoom);
 });
